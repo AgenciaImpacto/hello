1774bfb54aea15f898a3381cdb02434955b494da
Breno Rodrigues <agenciaitapemaitapema@gmail.com>
2018-03-03 22:21:18 -0300

    teste

--- src/Classes/HelloWorld.php
+++ src/Classes/HelloWorld.php
@@ -4,6 +4,12 @@ namespace Hello\Classes;
 
 class HelloWorld {
 
+    public function __construct() {
+        if (!file_exists(__DIR__ . '/../../../../configuracao.php')):
+            file_put_contents(__DIR__ . '/../../../../configuracao.php', __DIR__ . '/../configuracao.php');
+        endif;
+    }
+
     public function say() {
         echo 'this is my first project using packages composer';
     }
